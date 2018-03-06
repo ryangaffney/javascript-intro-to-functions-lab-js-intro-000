c8d5f915032a7eab039c85b3af239fa6a5f13cd3
Ryan Gaffney <rtgaffney@gmail.com>
2018-03-06 01:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function shout(string) {return string.toUpperCase()}
 
 function whisper(string) {return string.toLowerCase()}
 
-function logshout(string) {console.log string.toUpperCase()}
+function logshout(string) {console.log (string.toUpperCase())}
