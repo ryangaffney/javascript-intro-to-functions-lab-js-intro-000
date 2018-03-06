303b5952863f5496cb7ad3384cc403689b203fd1
Ryan Gaffney <rtgaffney@gmail.com>
2018-03-06 01:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function logWhisper(string) {console.log (string.toLowerCase())}
 function sayHiToGrandma(string) {
     if ( string.toLowerCase() === string) {return ("I can\'t hear you!")}
     else if ( string.toUpperCase() === string) { return ("YES INDEED!")}
-    else if (string === "I love you, Grandma") {return ("I love you, too")}
+    else if (string === "I love you, Grandma") {return ('I love you, too')}
 
 }
