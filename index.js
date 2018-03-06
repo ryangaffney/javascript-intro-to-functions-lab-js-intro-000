b3cb95c176ad55488b487a1142098d4ded3be4e9
Ryan Gaffney <rtgaffney@gmail.com>
2018-03-06 01:43:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function sayHiToGrandma(string) {
     if ( string.toLowerCase() === string) {return ("I can\'t hear you!")}
     else if ( string.toUpperCase() === string) { return ("YES INDEED")}
     else if (string === "I love you, Grandma") {return ("I love you, too")}
-    
+
 }
